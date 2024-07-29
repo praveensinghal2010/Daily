5345a4ce9b1ac8d38c3b318d47f30f717b1871d1
praveensinghal2010 <139489493+praveensinghal2010@users.noreply.github.com>
2024-07-29 19:08:40 +0530

    Update PrintingTest.java

--- src/test/java/temp1/PrintingTest.java
+++ src/test/java/temp1/PrintingTest.java
@@ -9,11 +9,18 @@ public class PrintingTest {
 
 	public void hellojava()
 	{
-		System.out.print("my code is  running");	
 		WebDriver driver=null;
-		WebDriverManager.chromedriver().setup();
-		driver= new ChromeDriver();
-		driver.manage().window().maximize();
+		ChromeOptions options= new ChromeOptions();
+		
+		URL abc= new URL("http://localhost:4444/wd/hub");
+		
+		driver= new RemoteWebDriver(abc,options);
+		
+//		System.out.print("my code is  running");	
+//		
+//		WebDriverManager.chromedriver().setup();
+//		driver= new ChromeDriver();
+//		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		//driver.close();
 	}
