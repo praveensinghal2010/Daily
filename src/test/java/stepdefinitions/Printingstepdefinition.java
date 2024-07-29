dac080633c2846f90ca45838014ece1862f3d262
praveensingal2010 <praveen.singhal2010@gmail.com>
2024-07-29 19:11:00 +0530

    again

--- src/test/java/stepdefinitions/Printingstepdefinition.java
+++ src/test/java/stepdefinitions/Printingstepdefinition.java
@@ -1,11 +1,13 @@
 package stepdefinitions;
 
+import java.net.MalformedURLException;
+
 import io.cucumber.java.en.Given;
 import temp1.PrintingTest;
 
 public class Printingstepdefinition {
 	@Given("that it is a scenario")
-	public void that_it_is_a_scenario() {
+	public void that_it_is_a_scenario() throws MalformedURLException {
 		PrintingTest pt= new PrintingTest();
 		pt.hellojava();
 	}
--- src/test/java/temp1/PrintingTest.java
+++ src/test/java/temp1/PrintingTest.java
@@ -1,19 +1,31 @@
 package temp1;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PrintingTest {
 
-	public void hellojava()
+	public void hellojava() throws MalformedURLException
 	{
-		System.out.print("my code is  running");	
 		WebDriver driver=null;
-		WebDriverManager.chromedriver().setup();
-		driver= new ChromeDriver();
-		driver.manage().window().maximize();
+		ChromeOptions options= new ChromeOptions();
+		
+		URL abc= new URL("http://localhost:4444/wd/hub");
+		
+		driver= new RemoteWebDriver(abc,options);
+		
+//		System.out.print("my code is  running");	
+//		
+//		WebDriverManager.chromedriver().setup();
+//		driver= new ChromeDriver();
+//		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		//driver.close();
 	}
